cebabc024eb754e096b1e74cc89de80cf16d0da6
fatma-brahmi <brahmifatma96@gmail.com>
2021-08-26 11:19:20 +0100

    react-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
 import './App.css';
+import EmailIcon from '@material-ui/icons/Email';
+import LockIcon from '@material-ui/icons/Lock';
+
 
 function App() {
-  return (
+  return ( 
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>login</h1>
+      <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className="label"><EmailIcon/> Email address <span>*</span></Form.Label>
+    <Form.Control  type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      <p>We'll never share your email with anyone else.</p>
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className="label"><LockIcon/>Password <span>*</span></Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button className="btn" variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
     </div>
   );
 }
